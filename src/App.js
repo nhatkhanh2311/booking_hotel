89abae771717b2f36c2fe8ae9e24e21627e0ae6b
unknown <leluan2047@gmail.com>
2021-04-21 20:37:59 +0700

    L up UpRoom

--- src/App.js
+++ src/App.js
@@ -4,20 +4,12 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Error from "./pages/Error";
+import UpHotel from "./components/UpHotel";
 
 export default function App() {
     return (
-        <Router>
-            <Header/>
-            <Switch>
-                <Route exact path="/">
-                    <Home/>
-                </Route>
-                <Route path="*">
-                    <Error/>
-                </Route>
-            </Switch>
-            <Footer/>
-        </Router>
+        <div>
+            <UpHotel></UpHotel>
+        </div>
     );
 }
---
+++ src/components/UpRoom.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { Container,FormGroup,Label,Input } from "reactstrap";
+import './css/UpRoom.css';
+
+
+export default function SignUp() {
+   
+    const onSubmit = (e) => {
+        e.preventDefault();
+
+      
+    }
+
+   
+
+    return (
+        <div className="UpRoom">
+            <form onSubmit={onSubmit}>
+
+                <div className="form-group">
+                    <label>Tên phòng: </label>
+                    <input type="text" className="form-control" placeholder="Nhập tên"
+                       
+                    />
+                  
+                </div>
+
+
+            
+                <div className="form-group">
+                    <label>Giá tiền</label>
+                    <input pattern="[0-9]" className="form-control" placeholder="Nhập giá"
+                        
+                    />
+                 
+
+                </div>
+               
+                <div className="form-group">
+                    <label>Mô tả</label>
+                    <textarea type="phone" className="form-control" placeholder="Nhập mô tả" />
+                </div>
+
+                <div className="form-group">
+                    <label>Tên khách sạn: </label>
+                    <input type="text" className="form-control" placeholder="Nhập tên khách sạn"
+                       
+                    />
+                  
+                </div>
+                <div className="form-group">
+                    <label>Loại phòng: </label>
+                    <input type="text" className="form-control" placeholder="Nhập loại"
+                       
+                    />
+                  
+                </div>
+                <div className="form-group">
+                    <Label>Tải hình ảnh phòng:</Label>
+                    <Input type="file" name="image" />
+                    <br></br>
+
+                </div>
+
+               
+
+                <button type="submit" className="btn btn-primary btn-block">Gửi</button>
+                
+            </form>
+        </div>
+    );
+}
