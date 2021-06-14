f9e0ef7f09386541ca0f65e51e3098180e7db564
DESKTOP-KMNS09Q\Admin <quangtuankc040999@gmail.com>
2021-06-14 20:19:19 +0700

    + commit css

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,93 @@
 import React from "react";
-import "./css/Header-Footer.css";
-import {Link} from "react-router-dom";
-import facebook from "../assets/facebook.svg";
+import logo from "../assets/logo.png";
+import "./css/Footer.css";
 
 export default function Footer() {
-    return (
-        <footer>
-            <div className="mastfoot">
-                <div className="inner-footer">
-                    <div className="row">
-                        <div className="follow col-12">
-                            {"Theo dõi trên "}
-                            <Link>
-                                <img src={facebook} width="22px"/>
-                            </Link>
-                        </div>
-                        <div className="col-12">
-                            <div className="row">
-                                <ul className="col-12">
-                                    <li>
-                                        <Link className="footer-link">
-                                            Về chúng tôi
-                                        </Link>
-                                    </li>
-                                    <li>
-                                        <Link className="footer-link">
-                                            Trợ giúp
-                                        </Link>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <footer>
+      <div className="footer-container">
+        <div className="footer-content about-us">
+          <div className="footer-title">ABOUT US</div>
+          <hr className="hr" />
+          <div className="f-content">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+              eveniet quibusdam porro molestias eligendi laborum sint amet
+              repellendus pariatur reiciendis praesentium numquam rem sed
+              nostrum, asperiores quaerat quam facere officiis.
+            </p>
+          </div>
+          <div className="f-logo">
+            <img id="logo" src={logo} alt="This is logo" />
+          </div>
+        </div>
+        <div className="footer-content social">
+          <div className="footer-contact">
+            <div className="footer-title">CONTACT INFOR</div>
+            <hr className="hr" />
+            <div className="f-content">
+              <ul className="list-infor">
+                <li className="infor">
+                  <i className="bx bx-envelope-open icon"></i>{" "}
+                  bookingHotelCNPM@gmail.com
+                </li>
+                <li className="infor">
+                  <i className="bx bxs-phone icon"></i> 0123456789
+                </li>
+                <li className="infor">
+                  <i className="bx bxs-map icon"></i>154 - Nguyen Luong Bang -
+                  Lien Chieu - Da Nang
+                </li>
+              </ul>
             </div>
-        </footer>
-    );
+          </div>
+          <div className="footer-connect">
+            <div className="footer-title">CONNECT WITH US</div>
+            <hr className="hr" />
+            <div className="f-content">
+              <ul className="list-infor connect">
+                <li className="infor">
+                  <a href="https://www.instagram.com/" target="_blank">
+                    <i className="bx bxl-instagram-alt icon"></i>
+                  </a>
+                </li>
+                <li className="infor">
+                  <a href="https://www.facebook.com/" target="_blank">
+                    <i className="bx bxl-facebook-square icon"></i>
+                  </a>
+                </li>
+                <li className="infor">
+                  <a href="https://twitter.com/?lang=vi" target="_blank">
+                    <i className="bx bxl-twitter icon"></i>
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+        <div className="footer-content payment">
+          <div className="footer-title">PAYMENT ACCEPTED</div>
+          <hr className="hr" />
+          <div className="f-content payment-content">
+            <span>VISA</span>
+            <span>PayPal</span>
+            <span>MasterCard</span>
+          </div>
+        </div>
+
+        <div className="footer-content recent-post">
+          <div className="footer-title">RECENT POST</div>
+          <hr className="hr" />
+          <div className="f-content recent-post-content">
+            <a href="#"> post1</a>
+            <a href="#"> post2</a>
+            <a href="#"> post3</a>
+            <a href="#"> post4</a>
+            <a href="#"> post5</a>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,95 +1,99 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./css/Header-Footer.css";
-import {Link, useHistory} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import logo from "../assets/logo.png";
-import {Button, Modal, ModalBody, ModalHeader} from "reactstrap";
+import { Button, Modal, ModalBody, ModalHeader } from "reactstrap";
 import Login from "./Login";
 import SignUp from "./SignUp";
 
 export default function Header() {
-    const history = useHistory();
-    const [login, setLogin] = useState(false);
-    const [signup, setSignup] = useState(false);
-    const toggleLogin = () => setLogin(!login);
-    const toggleSignup = () => setSignup(!signup);
+  const history = useHistory();
+  const [login, setLogin] = useState(false);
+  const [signup, setSignup] = useState(false);
+  const toggleLogin = () => setLogin(!login);
+  const toggleSignup = () => setSignup(!signup);
 
-    const toAccount = () => history.push('/account');
+  const toAccount = () => history.push("/account");
 
-    const logout = () => {
-        localStorage.removeItem('username');
-        localStorage.removeItem('roles');
-        localStorage.removeItem('accessToken');
-        localStorage.removeItem('tokenType');
-        window.location.reload();
-    }
+  const logout = () => {
+    localStorage.removeItem("username");
+    localStorage.removeItem("roles");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("tokenType");
+    window.location.reload();
+  };
 
-    return (
-        <header>
-            <nav className="navbar navbar-expand-md navbar-dark clearfix masthead">
-                <div className="navbar-brand mx-auto">
-                    <Link to="/">
-                        <img src={logo} border="0" id="logo"/>
-                    </Link>
-                </div>
-                <div className="collapse navbar-collapse navbar-horizontal-menu">
-                    <ul className="navbar-nav ml-auto">
-                        <li>
-                            <Link className="nav-link header-link">
-                                Địa điểm
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className="nav-link header-link">
-                                Trải nghiệm
-                            </Link>
-                        </li>
-                        {localStorage.getItem('roles') && (
-                            <>
-                                <li>
-                                    <Button color="success" onClick={toAccount}>
-                                        Thông tin
-                                    </Button>
-                                </li>
-                                <li>
-                                    <Button color="danger" onClick={logout}>
-                                        Đăng xuất
-                                    </Button>
-                                </li>
-                            </>
-                        )}
-                        {!localStorage.getItem('roles') && (
-                            <>
-                                <li>
-                                    <Button color="success" onClick={toggleLogin}>
-                                        Đăng nhập
-                                    </Button>
-                                    <Modal className="modal-dialog modal-dialog-centered" isOpen={login} toggle={toggleLogin}>
-                                        <ModalHeader>
-                                            <h2>Đăng nhập</h2>
-                                        </ModalHeader>
-                                        <ModalBody>
-                                            <Login/>
-                                        </ModalBody>
-                                    </Modal>
-                                </li>
-                                <li>
-                                    <Button color="success" onClick={toggleSignup}>
-                                        Đăng ký
-                                    </Button>
-                                    <Modal className="modal-dialog modal-dialog-centered" isOpen={signup} toggle={toggleSignup}>
-                                        <ModalHeader>
-                                            <h2>Đăng ký</h2>
-                                        </ModalHeader>
-                                        <ModalBody>
-                                            <SignUp/>
-                                        </ModalBody>
-                                    </Modal>
-                                </li>
-                            </>
-                        )}
-                    </ul>
-                </div>
-            </nav>
-        </header>
-    );
+  return (
+    <header id="head">
+      <nav className="navbar navbar-expand-md navbar-dark clearfix masthead">
+        <div className="navbar-brand mx-auto">
+          <Link to="/">
+            <img src={logo} border="0" id="logo" />
+          </Link>
+        </div>
+        <div className="collapse navbar-collapse navbar-horizontal-menu">
+          <ul className="navbar-nav ml-auto">
+            <li>
+              <Link className="nav-link header-link">Địa điểm</Link>
+            </li>
+            <li>
+              <Link className="nav-link header-link">Trải nghiệm</Link>
+            </li>
+            {localStorage.getItem("roles") && (
+              <>
+                <li>
+                  <Button color="success" onClick={toAccount}>
+                    Thông tin
+                  </Button>
+                </li>
+                <li>
+                  <Button color="danger" onClick={logout}>
+                    Đăng xuất
+                  </Button>
+                </li>
+              </>
+            )}
+            {!localStorage.getItem("roles") && (
+              <>
+                <li>
+                  <Button color="success" onClick={toggleLogin}>
+                    Đăng nhập
+                  </Button>
+                  <Modal
+                    className="modal-dialog modal-dialog-centered"
+                    isOpen={login}
+                    toggle={toggleLogin}
+                  >
+                    <ModalHeader>
+                      <h2>Đăng nhập</h2>
+                    </ModalHeader>
+                    <ModalBody>
+                      <Login />
+                    </ModalBody>
+                  </Modal>
+                </li>
+                <li>
+                  <Button color="success" onClick={toggleSignup}>
+                    Đăng ký
+                  </Button>
+                  <Modal
+                    className="modal-dialog modal-dialog-centered"
+                    isOpen={signup}
+                    toggle={toggleSignup}
+                  >
+                    <ModalHeader>
+                      <h2>Đăng ký</h2>
+                    </ModalHeader>
+                    <ModalBody>
+                      <SignUp />
+                    </ModalBody>
+                  </Modal>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
 }
